198d24aa0aebfb9e0ab3910025fde3722bacaeaa
mutshiv <vmutshinya@gmail.com>
2024-10-19 22:28:21 +0200

    forgotten

--- lua/vwm/init.lua
+++ lua/vwm/init.lua
@@ -4,59 +4,61 @@ local win = function(buf, opts)
     return vim.api.nvim_open_win(buf, true, opts)
 end
 
-local i_win
+local i_win, m_win
 local main_buf
 local i_buf
 
 M.main_win = function()
     local height = 30
-    local width = 100
+    local width = 160
+    Color_text()
 
     local opts = {
         relative = 'editor',
         border = 'rounded',
         title = 'Main Window',
-        row = math.floor((vim.o.columns - height) / 2),
-        col = math.floor((vim.o.lines - width) / 2),
+        row = math.floor((vim.o.lines - height) / 2),
+        col = math.floor((vim.o.columns - width) / 2),
         width = width,
         height = height,
     }
 
     main_buf = vim.api.nvim_create_buf(false, true)
 
-    _ = win(main_buf, opts)
-    --  vim.api.nvim_buf_set_lines(main_buf, -1, -1, false, { 'Main Window', 'Enter your Text here... ' })
+    m_win = win(main_buf, opts)
 
     vim.api.nvim_buf_set_option(main_buf, 'modifiable', true)
-
-    Color_text()
 end
 
 M.input_win = function()
     if i_win and vim.api.nvim_win_is_valid(i_win) then
         vim.api.nvim_set_current_win(i_win)
-        vim.nvim_win_set_cursor(i_win, { 1, 0 })
+        vim.api.nvim_win_set_cursor(i_win, { 1, 0 })
         vim.cmd("startinsert")
     else
+        local m_win_config = vim.api.nvim_win_get_config(m_win)
+        local m_win_h = m_win_config.height
+        local m_win_r = m_win_config.row
 
         local opts = {
             relative = 'editor',
             border = 'single',
             style = 'minimal',
             title = 'Input Window',
-            row = math.floor((vim.o.columns - 50) / 2),
-            col = math.floor((vim.o.lines - 1) / 2),
-            width = 50,
+            row = math.floor((m_win_h) / 2),
+            col = m_win_r + math.floor((m_win_config.width - 100) / 2),
+            width = 100,
             height = 1,
         }
+
         i_buf = vim.api.nvim_create_buf(false, true)
 
         i_win = vim.api.nvim_open_win(i_buf, false, opts)
         vim.api.nvim_buf_set_option(i_buf, 'modifiable', true)
 
-        print("The inspect output for input_win" .. tostring(vim.inspect(i_win)))
+        print("The inspect output for input_win " .. tostring(vim.inspect(i_win)))
 
-        vim.nvim_win_set_cursor(i_win, { 1, 0 })
+        vim.api.nvim_win_set_cursor(i_win, { 1, 0 })
         vim.cmd("startinsert")
     end
 end
@@ -66,8 +68,8 @@ M.send_to_main_win = function()
 
     if input_text and #input_text > 0 then
         vim.api.nvim_buf_set_option(main_buf, 'modifiable', true)
+        vim.api.nvim_buf_add_highlight(main_buf, -1, 'colouring', 1, 0, -1)
         vim.api.nvim_buf_set_lines(main_buf, -1, -1, false, { input_text })
-        --vim.api.nvim_buf_add_highlight(main_buf, -1, 'colouring', input_text, 0, -1)
         vim.api.nvim_buf_set_option(main_buf, 'modifiable', false)
 
         vim.api.nvim_win_close(i_win, true)
@@ -77,7 +79,8 @@ M.send_to_main_win = function()
 end
 
 Color_text = function()
-    vim.api.nvim_set_hl(0, 'colouring', { fg = '#ffffff', bg = '#000000', bold = true })
+    print 'Coloring the text'
+    vim.api.nvim_set_hl(0, 'colouring', { fg = '#DC123C', bg = '#FFFDD0', bold = true })
 end
 
 return M
