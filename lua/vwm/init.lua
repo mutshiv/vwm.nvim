5220d70476b7485aadc0d5b1784fe5b59b0c166b
mutshiv <vmutshinya@gmail.com>
2024-10-19 21:29:25 +0200

    we have the input going
    Accepting input
    and sending data back to the main window.

--- lua/vwm/init.lua
+++ lua/vwm/init.lua
@@ -1,48 +1,83 @@
 local M = {}
 
-Win = function(buf, opts)
-    vim.api.nvim_open_win(buf, 0, opts)
+local win = function(buf, opts)
+    return vim.api.nvim_open_win(buf, true, opts)
 end
 
-local buf = vim.api.nvim_create_buf(false, true)
+local i_win
+local main_buf
+local i_buf
 
-M.buildUI = function()
-    local height = 20
+M.main_win = function()
+    local height = 30
     local width = 100
 
     local opts = {
         relative = 'editor',
         border = 'rounded',
-        title = 'Calc',
+        title = 'Main Window',
         row = math.floor((vim.o.columns - height) / 2),
-        col = math.floor((vim.o.columns - width) / 2),
+        col = math.floor((vim.o.lines - width) / 2),
         width = width,
         height = height,
     }
 
-    Win(buf, opts)
-    Color_text()
-    vim.api.nvim_buf_set_lines(buf, 0, -1, false, { "Hear hear..." })
+    main_buf = vim.api.nvim_create_buf(false, true)
 
-    vim.ui.input({ prompt = 'Enter your text here...' }, function(input)
-        Draw_input(input)
-    end)
+    _ = win(main_buf, opts)
+    --  vim.api.nvim_buf_set_lines(main_buf, -1, -1, false, { 'Main Window', 'Enter your Text here... ' })
+
+    vim.api.nvim_buf_set_option(main_buf, 'modifiable', true)
+
+    Color_text()
 end
 
-Draw_input = function(input_text)
-    print("Draw input function called...")
-    if input_text then
-        vim.api.nvim_buf_set_lines(buf, -1, -1, false, { input_text })
-        local line_number = vim.api.nvim_buf_line_count(buf) - 1
-        vim.api.nvim_buf_add_highlight(buf, -1, 'colouring', line_number, 0, -1)
+M.input_win = function()
+    if i_win and vim.api.nvim_win_is_valid(i_win) then
+        vim.api.nvim_set_current_win(i_win)
+        vim.nvim_win_set_cursor(i_win, { 1, 0 })
+        vim.cmd("startinsert")
     else
-        print("No text provided")
+
+        local opts = {
+            relative = 'editor',
+            border = 'single',
+            style = 'minimal',
+            title = 'Input Window',
+            row = math.floor((vim.o.columns - 50) / 2),
+            col = math.floor((vim.o.lines - 1) / 2),
+            width = 50,
+            height = 1,
+        }
+        i_buf = vim.api.nvim_create_buf(false, true)
+
+        i_win = vim.api.nvim_open_win(i_buf, false, opts)
+        vim.api.nvim_buf_set_option(i_buf, 'modifiable', true)
+
+        print("The inspect output for input_win" .. tostring(vim.inspect(i_win)))
+
+        vim.nvim_win_set_cursor(i_win, { 1, 0 })
+        vim.cmd("startinsert")
+    end
+end
+
+M.send_to_main_win = function()
+    local input_text = vim.api.nvim_buf_get_lines(i_buf, 0, 1, false)[1]
+
+    if input_text and #input_text > 0 then
+        vim.api.nvim_buf_set_option(main_buf, 'modifiable', true)
+        vim.api.nvim_buf_set_lines(main_buf, -1, -1, false, { input_text })
+        --vim.api.nvim_buf_add_highlight(main_buf, -1, 'colouring', input_text, 0, -1)
+        vim.api.nvim_buf_set_option(main_buf, 'modifiable', false)
+
+        vim.api.nvim_win_close(i_win, true)
+    else
+        print("No text provided...")
     end
 end
 
 Color_text = function()
-    print("colouring function called...")
-    vim.api.nvim_set_hl(0, 'colouring', { fg = '#fff000', bg = '#fff660', bold = true })
+    vim.api.nvim_set_hl(0, 'colouring', { fg = '#ffffff', bg = '#000000', bold = true })
 end
 
 return M
--- plugin/trial.lua
+++ plugin/trial.lua
@@ -3,7 +3,11 @@ local vwm = require('vwm')
 vwm.setup = function(opts)
     opts = opts or {}
 
-    vim.api.nvim_set_keymap('n', opts.map or '<leader>z', '<cmd>lua require("vwm").buildUI()<CR>',
+    vim.api.nvim_set_keymap('n', opts.map or '<leader>zw', '<cmd>lua require("vwm").main_win()<CR>',
+        { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', opts.map or '<leader>zi', '<cmd>lua require("vwm").input_win()<CR>',
+        { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', opts.map or '<leader>zs', '<cmd>lua require("vwm").send_to_main_win()<CR>',
         { noremap = true, silent = true })
 end
 
